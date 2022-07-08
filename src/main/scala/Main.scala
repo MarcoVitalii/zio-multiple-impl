038a01f61b10ea15e691d2e988754e31bf36b3d7
Marco Vitali <ilmarcvit@gmail.com>
2022-07-08 18:15:50 +0200

    refactor with map of implementations into env

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -2,91 +2,79 @@ import zio.{ExitCode, Has, ULayer, URIO, ZIO, ZLayer}
 import zio.clock.Clock
 import zio.random.Random
 import zio.console._
-import zio.Ref
 
-trait CheckInterface {
-  def name: String
-  def execute(params: String): ZIO[Any, Throwable, Boolean]
+object CheckService {
+  trait Service {
+    def name: String
+    def execute(params: String): ZIO[Any, Throwable, Boolean]
+  }
+  type CheckMap = Map[String, Service]
+
+  //we could provide an empty map that has to be updated while before building the final env with zio magic.
+  lazy val liveAllImpl = ZLayer
+    .succeed(Map.empty[String, Service])
+    .flatMap(CheckMat.addImpl)
+    .flatMap(CheckExa.addImpl)
+
+  def get(name: String) =
+    ZIO
+      .service[CheckService.CheckMap]
+      .flatMap(_.get(name) match {
+        case None       => ZIO.fail(new RuntimeException(s"Missing implementation for $name"))
+        case Some(impl) => ZIO.succeed(impl)
+      })
 }
 
-/*to add a new implementation:
-  1. add the case class that extends the interface
-  2. create a layer for it that registers the impl on the checkService
-  3. add the layer to the final environment (both on the type of the env and into the inject)
- */
-case class CheckExa(random: Random.Service, console: Console.Service) extends CheckInterface {
+case class CheckExa(random: Random.Service, console: Console.Service) extends CheckService.Service {
   def name = "CheckExa"
 
   override def execute(params: String): ZIO[Any, Throwable, Boolean] = console.putStrLn(s"I'm exa $params").as(false)
 }
 
 object CheckExa {
-  val live =
-    ZLayer.fromServicesM[Random.Service, CheckService.Service, Console.Service, Any, Throwable, CheckExa] {
-      (random, check, console) =>
-        for {
-          impl <- ZIO.succeed(CheckExa(random, console))
-          _    <- check.register(impl.name, impl)
-        } yield impl
+  def addImpl(hasMap: Has[CheckService.CheckMap]) =
+    ZLayer.fromServices[Random.Service, Console.Service, CheckService.CheckMap] { (random, console) =>
+      val impl = CheckExa(random, console)
+      println("adding exa")
+      hasMap.get + (impl.name -> impl)
     }
+
 }
 
-case class CheckMat(clock: Clock.Service, console: Console.Service) extends CheckInterface {
+case class CheckMat(clock: Clock.Service, console: Console.Service) extends CheckService.Service {
   def name = "CheckMat"
 
   override def execute(params: String): ZIO[Any, Throwable, Boolean] = console.putStrLn(params).as(false)
 }
 
 object CheckMat {
-
-  val live =
-    ZLayer.fromServicesM[Clock.Service, CheckService.Service, Console.Service, Any, Throwable, CheckMat] {
-      (clock, check, console) =>
-        for {
-          impl <- ZIO.succeed(CheckMat(clock, console))
-          _    <- check.register(impl.name, impl)
-        } yield impl
+  def addImpl(hasMap: Has[CheckService.CheckMap]) =
+    ZLayer.fromServices[Clock.Service, Console.Service, CheckService.CheckMap] { (clock, console) =>
+      val impl = CheckMat(clock, console)
+      println("adding mat")
+      hasMap.get + (impl.name -> impl)
     }
+
 }
 
-object CheckService {
-  type CheckService = Has[Service]
-  trait Service {
-    def register(name: String, impl: CheckInterface): ZIO[Any, Throwable, Unit]
-    def toImpl(name: String): ZIO[Any, Throwable, CheckInterface]
-  }
-
-  final case class Impl(ref: Ref[Map[String, CheckInterface]], console: Console.Service) extends Service {
-
-    def register(name: String, impl: CheckInterface) =
-      console.putStrLn(s"adding $name") *> ref.update(_ + (name -> impl))
-
-    def toImpl(name: String): ZIO[Any, Throwable, CheckInterface] = ref.get.map(_(name))
-
-  }
-  val live = ZLayer.fromServiceM[Console.Service, Any, Throwable, Service] { console =>
-    Ref.make(Map.empty[String, CheckInterface]).map(ref => Impl(ref, console))
-  }
-  def toImpl(name: String): ZIO[CheckService, Throwable, CheckInterface] = ZIO.serviceWith(_.toImpl(name))
-}
 case class EventToHandle(how: String, params: String)
 
 object main extends zio.App {
   import zio.magic._
   override def run(args: List[String]): URIO[zio.ZEnv, ExitCode] = {
 
-    type requiredEnv = Has[CheckService.Service] with Has[CheckExa] with Has[CheckMat] with Console
+    type requiredEnv = Has[CheckService.CheckMap] with Console
 
     val toProcess = List(EventToHandle("CheckMat", "doing mat"), EventToHandle("CheckExa", ", hello exa!"))
     val prog: ZIO[requiredEnv, Throwable, Unit] = ZIO.foreach_(toProcess) { ev =>
       for {
+        impl <- CheckService.get(ev.how)
         _    <- putStrLn(s"processing $ev")
-        impl <- CheckService.toImpl(ev.how)
         _    <- impl.execute(ev.params)
         _    <- putStrLn(s"done")
       } yield ()
     }
-    prog.injectCustom(CheckService.live, CheckMat.live, CheckExa.live).exitCode
-  }
+    prog.injectCustom(CheckService.liveAllImpl).exitCode
 
+  }
 }
